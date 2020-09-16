5e09ad267384aa951db0993ebdea1cc58b3ac2ee
Guilherme Bragamonte <gbragamonte@gmail.com>
2020-09-16 14:59:33 -0300

    [ADD] - Jwt as token

--- mongodb.js
+++ mongodb.js
@@ -1,9 +1,12 @@
 const { MongoClient } = require('mongodb')
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid')
+const jwt = require('jsonwebtoken')
 
 const url = `mongodb://localhost:27017`
 
 const client = new MongoClient(url)
+
+const jwtPassWord = 'role21'
 
 async function run() {
   await client.connect()
@@ -28,7 +31,9 @@ async function insertUser(user = {}) {
 
   const dateNow = new Date()
   const _id = uuidv4()
-  const token = uuidv4()
+
+  const { email } = user
+  const token = jwt.sign({ email }, jwtPassWord)
 
   await collection.insertOne({
     ...user,
@@ -47,7 +52,10 @@ async function loginUser(user = {}) {
   const database = client.db('sky-test')
   const collection = database.collection('users')
   const dateNow = new Date()
-  const token = uuidv4()
+
+  const { email } = user
+  const token = jwt.sign({ email }, jwtPassWord)
+
   await collection.updateOne(user, { $set: { token, ultimo_login: dateNow } })
   return true
 }
