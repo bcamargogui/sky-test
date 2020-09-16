e1fa292da0228dc795ae71a8098ecaf5ff0a9bb7
Guilherme Bragamonte <gbragamonte@gmail.com>
2020-09-16 11:24:16 -0300

    [ADD] - Generic error response function

--- index.js
+++ index.js
@@ -9,12 +9,19 @@ function response(resource = {}, data = {}, status = 200) {
   resource.status(status).send(data)
 }
 
+function responseError(
+    resource = {},
+    mensagem = 'Ops, algo deu errado, tente novamente mais tarde!') {
+  resource.status(500).send({ mensagem })
+}
+
 // parse application/json
 app.use(bodyParser.json())
 
 // routes
 app.get('/', (req, res) => {
-  response(res, { success: true })
+  // response(res, { success: true })
+  responseError(res, 'Ops, algo deu errado')
 })
 
 app.post('/', (req, res) => {
