19a3404165e3b720b6c040a513f8fce6395e2136
Guilherme Bragamonte <gbragamonte@gmail.com>
2020-09-16 13:46:22 -0300

    [ADD] - Sign-up routine

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require('express')()
 const bodyParser = require('body-parser')
+const { fetchUser, insertUser } = require('./mongodb')
 
 // constants
 const port = 3000
@@ -20,8 +21,39 @@ app.use(bodyParser.json())
 
 // routes
 app.get('/', (req, res) => {
-  // response(res, { success: true })
-  responseError(res, 'Ops, algo deu errado')
+  response(res, { online: true })
+})
+
+async function responseUser(user = {}) {
+  const {
+    _id,
+    data_criacao,
+    data_atualizacao,
+    ultimo_login,
+    token,
+  } = await fetchUser(user)
+
+  return {
+    data_criacao,
+    data_atualizacao,
+    ultimo_login,
+    token,
+    id: _id,
+  }
+}
+
+app.post('/sign-up', async (req, res) => {
+  try {
+    const { nome, email, senha, telefones } = req.body
+    await insertUser({ nome, email, senha, telefones })
+    const user = await responseUser({ email })
+    response(res, user)
+  } catch (error) {
+    const { code, message } = error
+    const duplicateKeyError = 11000
+    if (code !== duplicateKeyError) responseError(res, message)
+    responseError(res, 'E-mail já existente')
+  }
 })
 
 app.post('/', (req, res) => {
---
+++ mongodb.js
@@ -0,0 +1,49 @@
+const { MongoClient } = require('mongodb')
+const { v4: uuidv4 } = require('uuid');
+
+const url = `mongodb://localhost:27017`
+
+const client = new MongoClient(url)
+
+async function run() {
+  await client.connect()
+  const database = client.db('sky-test')
+  const collection = database.collection('users')
+  await collection.createIndex( { 'email': 1 }, { unique: true } )
+}
+
+run().catch(console.dir)
+
+async function fetchUser(user = {}) {
+  await client.connect()
+  const database = client.db('sky-test')
+  const collection = database.collection('users')
+  const findUser = await collection.findOne(user)
+  return findUser
+}
+
+async function insertUser(user = {}) {
+  await client.connect()
+  const database = client.db('sky-test')
+  const collection = database.collection('users')
+
+  const dateNow = new Date()
+  const _id = uuidv4()
+  const token = uuidv4()
+
+  await collection.insertOne({
+    ...user,
+    _id,
+    token,
+    data_criacao: dateNow,
+    data_atualizacao: dateNow,
+    ultimo_login: dateNow,
+  })
+
+  return true
+}
+
+module.exports = {
+  fetchUser,
+  insertUser,
+}
